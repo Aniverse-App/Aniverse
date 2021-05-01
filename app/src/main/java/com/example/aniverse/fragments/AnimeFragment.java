59cfaf48529ed8176432507ea20187de69590e09
Ezequiel Soler <esoler19@gmail.com>
2021-05-01 13:18:44 -0400

    Anime Screen Nearly Complete

--- app/src/main/java/com/example/aniverse/fragments/AnimeFragment.java
+++ app/src/main/java/com/example/aniverse/fragments/AnimeFragment.java
@@ -13,7 +13,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ProgressBar;
 import android.widget.SearchView;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
